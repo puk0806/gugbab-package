2fe2eb046ec19633a321384f7e589d9d1f88d091
puk0806 <puk0806@naver.com>
2025-06-08 16:14:44 +0900

    [all] Modify Fommatting Biome

--- packages/utils/src/lib/parse.ts
+++ packages/utils/src/lib/parse.ts
@@ -1,4 +1,4 @@
-export const pxToRem = (px: `${number}px`, baseFont: number = 16) => {
+export const pxToRem = (px: `${number}px`, baseFont = 16) => {
   const [_px] = px.split('px');
   return `${Number(_px) / baseFont}rem`;
 };
