b211e16046fc8d81a115d64da327970cf8b9bcc3
puk0806 <puk0806@naver.com>
2025-06-08 15:38:22 +0900

    [all] Modify Git Hook Eslint -> Biome

--- .githooks/pre-push.py
+++ .githooks/pre-push.py
@@ -19,26 +19,26 @@ def lint_files(modified_files):
     ]
 
     if not files:
-        print("Lint 대상 파일이 없습니다.")
+        print("Biome 대상으로 처리할 파일이 없습니다.")
         return
 
     try:
         for file in files:
-            print(f"Linting {file} ...")
-            subprocess.run(f"npx eslint --fix '{file}'", shell=True, check=True)
+            print(f"Biome 적용 중: {file}")
+            subprocess.run(f"npx biome check --apply '{file}'", shell=True, check=True)
 
         files_str = " ".join(f"'{file}'" for file in files)
         subprocess.run(f"git add {files_str}", shell=True, check=True)
 
-        print("모든 파일에 대해 ESLint 검사가 완료되었습니다.")
+        print("모든 파일에 대해 Biome 포맷 및 린트 검사가 완료되었습니다.")
     except subprocess.CalledProcessError as e:
-        print(f"ESLint 실행 중 오류 발생: {e}")
+        print(f"Biome 적용 중 오류 발생: {e}")
         sys.exit(e.returncode)
 
 
 def type_check():
     try:
-        print("Running global type check...")
+        print("타입 검사 실행 중...")
         result = subprocess.run("pnpm run typecheck", shell=True, cwd=BASE_PATH)
         return result.returncode
     except subprocess.CalledProcessError as e:
